300e08d0b6d7c1add23615c4760259335d381de1
Dilmurod Valiev <valiev.dilmurod1998@gmail.com>
2022-05-06 18:19:37 +0900

    Refactor code (testing with json)

--- Assets/Scripts/ScriptExecutor.cs
+++ Assets/Scripts/ScriptExecutor.cs
@@ -18,6 +18,11 @@ public class ScriptExecutor : MonoBehaviour
 
     private void Start()
     {
+        Context.Instance.CurrentFigureID = "402-32-11-61-990-802-A";
+        Context.Instance.CurrentTaskID = "1";
+        Context.Instance.CurrentSubtaskID = "2";
+        Context.Instance.CurrentInstructionOrder = "3";
+        
         var textObjects = FindObjectsOfType<Text>();
 
         foreach (var textObj in textObjects)
@@ -77,11 +82,6 @@ public class ScriptExecutor : MonoBehaviour
                 };
             }
         }
-
-        Context.Instance.CurrentFigureID = "402-32-11-61-990-802-A";
-        Context.Instance.CurrentTaskID = "1";
-        Context.Instance.CurrentSubtaskID = "2";
-        Context.Instance.CurrentInstructionOrder = "3";
     }
 
     private void Update()
